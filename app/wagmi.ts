6ba89a6f68230329557dcbb5d1a0c75e52eb5393
Vadims Kokins <94623385+KUNZIT@users.noreply.github.com>
2025-12-05 10:31:57 +0100

    Update wagmi.ts

--- app/wagmi.ts
+++ app/wagmi.ts
@@ -1,9 +1,9 @@
 import { http, createConfig, cookieStorage, createStorage } from 'wagmi';
-import { sepolia } from 'wagmi/chains';
+import { baseSepolia } from 'wagmi/chains';
 import { QueryClient } from '@tanstack/react-query';
 
 // 1. Define chains
-export const chains = [sepolia] as const;
+export const chains = [baseSepolia] as const;
 
 // 2. Create Pure Wagmi Config (No Web3Modal wrapper)
 export const config = createConfig({
@@ -13,7 +13,7 @@ export const config = createConfig({
     storage: cookieStorage
   }),
   transports: {
-    [sepolia.id]: http(process.env.NEXT_PUBLIC_RPC_URL)
+    [baseSepolia.id]: http(process.env.NEXT_PUBLIC_RPC_URL)
   },
 });
 
